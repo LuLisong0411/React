7c774325ad59421e161f1d57797a842928ce3159
陆力松 <lulisong0411@163.com>
2019-08-02 16:39:07 +0800

    最简单的基础组件

--- src/main.js
+++ src/main.js
@@ -8,21 +8,33 @@ import ReactDom from 'react-dom'
 //创建dom对象
 // var H1 = React.createElement('h1',null,'这是一个H1');
 // var Div = React.createElement('div',{title:'this is a div',id:'div'},'这是一个div',H1)
-var title = "这是使用变量定义的值"
-var arr = [];
-for(var i = 0; i < 10; i++){ 
-    var p = <p className="Div" key={i}>JSX 真好用</p>
-    arr.push(p)
-}
+// var title = "这是使用变量定义的值"
+// var arr = [];
+// for(var i = 0; i < 10; i++){ 
+//     var p = <p className="Div" key={i}>JSX 真好用</p>
+//     arr.push(p)
+// }
 
-var Div = <div>
-    这是使用了 JSX 语法创建的 Div。
-    <p className="Div">JSX 真好用</p>
-    <h1 title={ title + '，符合js代码的编译都是可以的' }>要清楚 JSX 的本质原理</h1>
-    <label htmlFor=""></label>
-    {arr}
-</div>
+// var Div = <div>
+//     这是使用了 JSX 语法创建的 Div。
+//     <p className="Div">JSX 真好用</p>
+//     <h1 title={ title + '，符合js代码的编译都是可以的' }>要清楚 JSX 的本质原理</h1>
+//     <label htmlFor=""></label>
+//     {arr}
+// </div>
 //React.render('要渲染的虚拟 DOM 元素','要渲染到页面上的哪个位置中')
 //***坑：ReactDom.render() 方法的第二个参数和 vue 不一样，不接收 "#app" 这样的字符串，而是需要传递一个原生的 DOM 对象。
- 
-ReactDom.render(Div,document.getElementById('app'))
+//在 React 中，构造函数就是一个最基本的组件。 
+// 如果想把组件放到页面中，可以把构造函数的名称当作组件的名称，以 html 的形式引入页面即可
+//***坑：React 在解析所有的标签的时候是以标签的首字母来区分的。
+// 如果标签的首字母是小写，那么按照普通的 html 标签来解析；如果首字母是大写则按照组件的形式去解析。
+//组件的首字母必须大写
+ function Hello(){
+     return <div>
+         <h1>这是在hello组件中定义的元素</h1>
+     </div>
+ }
+// ReactDom.render(Div,document.getElementById('app'))
+ReactDom.render(<div>
+    <Hello></Hello>
+</div>,document.getElementById('app'))
